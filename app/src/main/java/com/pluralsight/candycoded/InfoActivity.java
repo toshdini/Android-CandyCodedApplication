6474f85310c8630dc733c51a13b48d4a64f32b2e
abdirahman Mohamed <toahdini.1999@gmail.com>
2021-09-11 11:03:19 -0700

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -36,8 +36,8 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     public void createMapIntent(View view) {
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri).setPackage(
-                "com.google.android.apps.maps");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
         if(mapIntent.resolveActivity(getPackageManager()) != null){
             startActivity(mapIntent);
         }
