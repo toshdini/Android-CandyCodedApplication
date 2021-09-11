949789e580a04f77bbcb07e4c91ac2ecdd7821a2
abdirahman Mohamed <toahdini.1999@gmail.com>
2021-09-11 11:20:23 -0700

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -76,11 +76,11 @@ public class DetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        createSharedIntent();
+        createShareIntent();
         return super.onOptionsItemSelected(item);
     }
 
-    private void createSharedIntent() {
+    private void createShareIntent() {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_TEXT,
